7f60c435b41701cdb3d456d71293e223e43b6ed6
gwebsurfer <gab@gab.art.br>
2024-01-16 05:55:17 -0300

    fix: adjust href

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className='bg-white shadow-md fixed w-full z-10 top-0 left-0'>
       <div className='flex justify-center items-center h-16'>
-        <a href='/' aria-label='Voltar para a Home'>
+        <a href='/psicomanager/' aria-label='Voltar para a Home'>
           <img src={LogoImage} alt='Logo' role='img' className='h-10 w-auto' />
         </a>
       </div>
